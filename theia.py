7403be003817bf288327a927c7fc9d4ed4d71a90
Amanara <52249849+4m4Sec@users.noreply.github.com>
2022-08-18 16:45:10 +0200

    update [1.2.0] : fix error when arg missing

--- theia.py
+++ theia.py
@@ -11,14 +11,17 @@ def main():
     print(colors.BOLD + colors.HEADER + get_ascii())
     parse_arguments()
 
-    if sys.argv[1] == "-a" or sys.argv[1] == "--adress":
-        print(colors.OKBLUE + "┌───── " + colors.HEADER + "IP-Adress lookup [" + sys.argv[2] + "]" + colors.OKBLUE + " ─────┐")
-        data = req_ip(sys.argv[2])
+    try:
+        if sys.argv[1] == "-a" or sys.argv[1] == "--adress":
+            print(colors.OKBLUE + "┌───── " + colors.HEADER + "IP-Adress lookup [" + sys.argv[2] + "]" + colors.OKBLUE + " ─────┐")
+            data = req_ip(sys.argv[2])
 
-        for key in data:
-            if key in forbidden_keys: continue
-            print(colors.OKCYAN + "[+] " + colors.OKBLUE + key.capitalize() + colors.ENDC + " -> " + colors.OKGREEN + str(data[key]))
-        print(colors.OKBLUE + "└───── " + colors.HEADER + "IP-Adress lookup [" + sys.argv[2] + "]" + colors.OKBLUE + " ─────┘" + "\n")
+            for key in data:
+                if key in forbidden_keys: continue
+                print(colors.OKCYAN + "[+] " + colors.OKBLUE + key.capitalize() + colors.ENDC + " -> " + colors.OKGREEN + str(data[key]))
+            print(colors.OKBLUE + "└───── " + colors.HEADER + "IP-Adress lookup [" + sys.argv[2] + "]" + colors.OKBLUE + " ─────┘" + "\n")
+    except:
+        os.system("python theia.py --help")
 
 if __name__ == "__main__":
     main()
