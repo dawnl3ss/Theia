73775afbe49e38845e41c7b692decfb713d02692
Restart <45609733+RestartFU@users.noreply.github.com>
2024-10-08 17:57:46 -0400

    Update theia.sh

--- theia.sh
+++ theia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(command -v python)" == "" ]; then
-  echo "wget is required to use this"
+  echo "python is required to use this"
   echo "download using:"
   echo "  arch:   sudo pacman -S python"
   echo "  debian: sudo apt install python"
